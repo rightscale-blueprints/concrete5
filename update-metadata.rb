c745dcd589f9b578a9a6923ec3d132c377d4ca37
Chris Fordham <chris@xhost.com.au>
2013-04-22 13:49:09 +1000

    Add in removal of runit .gitignore.

--- update-metadata.rb
+++ update-metadata.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/ruby
 
+# first, manually remove some .gitignore files in cookbooks
+system("[ -e cookbooks/runit/.gitignore ] && rm -v cookbooks/runit/.gitignore")
+
 Dir.entries('cookbooks').each { |cookbook|
   system("knife cookbook metadata #{cookbook} -o cookbooks/")
 }
